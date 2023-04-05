f327be4b0122410c91f56449fcce5e75bac6c323
shimronraj <99860280+shimronraj@users.noreply.github.com>
2023-04-06 00:31:20 +0530

    Delete bookfast directory

--- bookfast/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(login)
-admin.site.register(registartion)
-admin.site.register(hospital)
-admin.site.register(doctors)
-admin.site.register(appointment)
-admin.site.register(Text)
